a9ba6189ea928b7cd36351604efd3fe845bf4534
JosueCarrascoH <89035938+JosueCarrascoH@users.noreply.github.com>
2023-09-29 17:12:08 -0500

    fix perfil

--- app/src/main/java/com/example/fidofriend/MainActivity.kt
+++ app/src/main/java/com/example/fidofriend/MainActivity.kt
@@ -1,72 +1,14 @@
 package com.example.fidofriend
 
-import android.content.Intent
-import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-import android.view.MenuItem
 
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
-
-class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
-    private lateinit var drawer: DrawerLayout
-    private lateinit var toggle: ActionBarDrawerToggle
-
-
-
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar: androidx.appcompat.widget.Toolbar=findViewById(R.id.toolbar_main)
-        setSupportActionBar(toolbar)
-
-        drawer=findViewById(R.id.drawer_layout)
-
-        toggle= ActionBarDrawerToggle(this,drawer,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer.addDrawerListener(toggle)
-
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setHomeButtonEnabled(true)
-
-        val navigationView: NavigationView =findViewById(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener(this)
-
-
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-
-            R.id.nav_item_one-> {
-                val intent = Intent(this, Profile::class.java)
-                startActivity(intent)
-            }
-        }
-        drawer.closeDrawer(GravityCompat.START)
-        return true
-    }
-
-    override fun onPostCreate(savedInstanceState: Bundle?) {
-        super.onPostCreate(savedInstanceState)
-        toggle.syncState()
-    }
-
-    override fun onConfigurationChanged(newConfig: Configuration) {
-        super.onConfigurationChanged(newConfig)
-        toggle.onConfigurationChanged(newConfig)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
     }
 
 
