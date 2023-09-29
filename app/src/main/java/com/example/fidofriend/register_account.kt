5412c44f3ccd08af431725c753691928eb9f9abc
AlfredoFarro <u201911924@upc.edu.pe>
2023-09-29 18:36:58 -0500

    Adding Register_Account Activity

--- app/src/main/java/com/example/fidofriend/register_account.kt
+++ app/src/main/java/com/example/fidofriend/register_account.kt
@@ -3,9 +3,9 @@ package com.example.fidofriend
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class register_account : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register_account)
     }
 }
