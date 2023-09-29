2b90f17ca24262eef04120d8ea8fbc94db493b7e
Sergio Bazan <drakeb2008@hotmail.com>
2023-09-29 17:51:39 -0500

    add menu

--- app/src/main/java/com/example/fidofriend/MainActivity.kt
+++ app/src/main/java/com/example/fidofriend/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.fidofriend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Configurar el Toolbar como ActionBar
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.nav_profile -> Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+            R.id.nav_home -> Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+            R.id.nav_appointments -> Toast.makeText(this, "Appointments", Toast.LENGTH_SHORT).show()
+            R.id.nav_service -> Toast.makeText(this, "Services", Toast.LENGTH_SHORT).show()
+            R.id.nav_products -> Toast.makeText(this, "Products", Toast.LENGTH_SHORT).show()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
